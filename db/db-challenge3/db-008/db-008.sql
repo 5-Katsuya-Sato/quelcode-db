58faf041ecacae47ebf6913ce34943c8e7691caa
Satu <kttth0320@gmail.com>
2020-10-01 17:34:10 +0900

    年月日表示方法の変更

--- db/db-challenge3/db-008/db-008.sql
+++ db/db-challenge3/db-008/db-008.sql
@@ -1,6 +1,6 @@
 SELECT u.name
        , c.name
-       , CAST(cu.joined_at AS DATE) AS joined_date 
+       , DATE_FORMAT(cu.joined_at, '%Y %m %d') AS joined_date
 FROM chat_users cu 
      JOIN users u 
      ON cu.user_id = u.id 
@@ -8,5 +8,4 @@ FROM chat_users cu
      JOIN chats c 
      ON cu.user_id = c.id 
         AND c.is_deleted = 0 
-WHERE substring(cu.joined_at, 1, 10)
 ORDER BY cu.joined_at;
