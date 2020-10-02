3a2a0edbc2ed3e51be376ed56af1df5b0decdc32
Satu <kttth0320@gmail.com>
2020-10-02 17:41:24 +0900

    013 or の記述追加

--- db/db-challenge3/db-013/db-013.sql
+++ db/db-challenge3/db-013/db-013.sql
@@ -1,7 +1,8 @@
 BEGIN;
 UPDATE users 
 SET is_deleted=1 
-WHERE number_work IS NULL 
-      AND number_private IS NULL 
+WHERE (number_work IS NULL OR number_work="")
+      AND
+      (number_private IS NULL OR number_private="")
       AND is_deleted=0;
 COMMIT;
